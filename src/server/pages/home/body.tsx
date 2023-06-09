732bf7be2e94af21d76a27bbf1db1deb01212af1
Jack Bliss <jack@battlefy.com>
2023-06-09 12:00:47 +0100

    removed unused file and restored page content

--- src/server/pages/home/body.tsx
+++ src/server/pages/home/body.tsx
@@ -3,7 +3,7 @@ import { render } from 'preact-render-to-string';
 export function body() {
   return render(
     <>
-      <div>Another deployed thing!!</div>
+      <div>Rendered on the server</div>
       <img src="/nausicaa.png" />
       <div id="root"></div>
     </>,
